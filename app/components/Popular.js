5e61b93994b888582340c07031b75b9f55dc72d4
Viktor Zhurbin <vzhurbin@gmail.com>
2018-05-19 20:53:50 +0300

    as far as I can get

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,44 +1,32 @@
 import React from 'react';
 
 class Popular extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedLanguage: 'All',
-    };
+  state = {
+    selectedLanguage: 'All',
+  };
 
-    this.updateLanguage = this.updateLanguage.bind(this);
-  }
-
-  updateLanguage(lang) {
-    this.setState(function() {
-      return {
-        selectedLanguage: lang,
-      };
-    });
-  }
+  updateLanguage = lang => {
+    this.setState({ selectedLanguage: lang });
+  };
 
   render() {
-    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+    const { selectedLanguage } = this.state;
 
     return (
       <div>
         <ul className="languages">
-          {languages.map(function(lang) {
+          {languages.map(lang => {
             return (
               <li
-                style={
-                  lang === this.state.selectedLanguage
-                    ? { color: '#d0021b' }
-                    : null
-                }
-                onClick={this.updateLanguage.bind(null, lang)}
+                style={lang === selectedLanguage ? { color: '#d0021b' } : null}
+                onClick={() => this.updateLanguage(lang)}
                 key={lang}
               >
                 {lang}
               </li>
             );
-          }, this)}
+          })}
         </ul>
       </div>
     );
