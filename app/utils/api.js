893fe89f13cee9e4d3f84a62f61556d53d80f15f
Viktor Zhurbin <vzhurbin@gmail.com>
2018-05-24 21:57:01 +0300

    update api

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,29 +1,23 @@
 var axios = require('axios');
 
-var id = 'CLIENT_ID';
-var secret = 'CLIENT_SECRET';
-var params = '&client_id=' + id + '&client_secret=' + secret;
+var id = "YOUR_CLIENT_ID";
+var sec = "YOUR_SECRET_ID";
+var params = "?client_id=" + id + "&client_secret=" + sec;
 
 function getProfile(username) {
-  return axios
-    .get('https://api.github.com.users/' + username /* + params */) // add params if you're out of requests limit
-    .then(function(user) {
+  return axios.get('https://api.github.com/users/' + username + params)
+    .then(function (user) {
       return user.data;
     });
 }
 
 function getRepos(username) {
-  return axios.get(
-    'https://api.github.com.users/' +
-    username +
-    '/repos' /* + params */ +
-      '&per_page=100',
-  );
+  return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100');
 }
 
 function getStarCount(repos) {
-  return repos.data.reduce(function(count, repo) {
-    return count + repo.stargazer_count;
+  return repos.data.reduce(function (count, repo) {
+    return count + repo.stargazers_count
   }, 0);
 }
 
@@ -31,7 +25,7 @@ function calculateScore(profile, repos) {
   var followers = profile.followers;
   var totalStars = getStarCount(repos);
 
-  return followers * 3 + totalStars;
+  return (followers * 3) + totalStars;
 }
 
 function handleError(error) {
@@ -40,39 +34,38 @@ function handleError(error) {
 }
 
 function getUserData(player) {
-  axios.all([getProfile(player), getRepos(player)]).then(function(data) {
+  return axios.all([
+    getProfile(player),
+    getRepos(player)
+  ]).then(function (data) {
     var profile = data[0];
     var repos = data[1];
 
     return {
       profile: profile,
-      score: calculateScore(profile, repos),
-    };
+      score: calculateScore(profile, repos)
+    }
   });
 }
 
 function sortPlayers(players) {
-  return players.sort(function(a, b) {
+  return players.sort(function (a, b) {
     return b.score - a.score;
   });
 }
 
 module.exports = {
-  battle: function(players) {
-    return axios
-      .all(players.map(getUserData))
+  battle: function (players) {
+    return axios.all(players.map(getUserData))
       .then(sortPlayers)
       .catch(handleError);
   },
-  fetchPopularRepos: function(language) {
-    var encodedURI = window.encodeURI(
-      'https://api.github.com/search/repositories?q=stars:>1+language:' +
-        language +
-        '&sort=stars&order=desc&type=Repositories',
-    );
+  fetchPopularRepos: function (language) {
+    var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
 
-    return axios.get(encodedURI).then(function(response) {
-      return response.data.items;
-    });
-  },
+    return axios.get(encodedURI)
+      .then(function (response) {
+        return response.data.items;
+      });
+  }
 };
