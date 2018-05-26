45fd6108937d4598dfced5c932b74b57dd111ab7
Viktor Zhurbin <vzhurbin@gmail.com>
2018-05-26 10:05:11 +0300

    ready to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
-module.exports = {
+var config = {
   entry: ['babel-polyfill', './app/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -41,3 +42,15 @@ module.exports = {
     }),
   ],
 };
+
+if (process.env.NODE_ENV === 'production') {
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+  );
+}
+
+module.exports = config;
